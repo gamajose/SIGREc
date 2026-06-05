e74c2a5f4c4e748ebb69221b10a21569305ca80c
jose luiz gama <115404896+gamajose@users.noreply.github.com>
2026-06-05 18:39:02 -0300

    feat: permitir busca de usuario por codigo

--- src/main/java/br/gov/sigrec/tfdapac/repository/UserAccountRepository.java
+++ src/main/java/br/gov/sigrec/tfdapac/repository/UserAccountRepository.java
@@ -2,11 +2,21 @@ package br.gov.sigrec.tfdapac.repository;
 
 import br.gov.sigrec.tfdapac.domain.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
     Optional<UserAccount> findByUsername(String username);
+
+    @Query("""
+            select u
+            from UserAccount u
+            where lower(u.username) = lower(:login)
+               or lower(u.codigoUsuario) = lower(:login)
+            """)
+    Optional<UserAccount> findByUsernameOrCodigoUsuario(@Param("login") String login);
 
     boolean existsByUsername(String username);
 }
